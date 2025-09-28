8146dc9a1a331c2d8a033ce205305341aa25b51a
Guilherme Rezende <guilhermearesende1@gmail.com>
2025-09-28 14:12:37 -0300

    feat: first commit

---
+++ src/main/java/br/com/guiarese/persons/PersonsApplication.java
@@ -0,0 +1,13 @@
+package br.com.guiarese.persons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PersonsApplication.class, args);
+	}
+
+}
